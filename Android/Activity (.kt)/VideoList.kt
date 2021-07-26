dfb0049eb5f45cfb0397527f7187c3e8cc490589
김재원 <i7can7i@gmail.com>
2021-07-27 01:33:05 +0900

    Replace VideoList.kt

--- Android/Activity (.kt)/VideoList.kt
+++ Android/Activity (.kt)/VideoList.kt
@@ -3,24 +3,27 @@ package com.example.catcha
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.catcha.databinding.ActivityMainBinding
 import com.example.catcha.databinding.VideoListBinding
 
 class VideoList : AppCompatActivity() {
-    private var mBinding: VideoListBinding? = null
-    private val binding get() = mBinding!!
+    private lateinit var binding: VideoListBinding
+    private val cctvList: ArrayList<List<Cctvs>> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding = VideoListBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        /*mBinding = VideoListBinding.inflate(layoutInflater)
+        setContentView(binding.root)*/
 
-        binding.userSidePage.setOnClickListener{
+        /*binding.userSidePage.setOnClickListener{
             val intent = Intent(this, UserSidePage::class.java)
             startActivity(intent)
-        }
+        }*/
 
-        val cctvList = arrayListOf(
+        /*val cctvList = arrayListOf(
             // 지금은 예시로 써놓은거고 나중에 서버에서 받아들인 데이터가 표시되도록 바꿔야함
             Cctvs(R.drawable.sample2, 2, 20210708),
             Cctvs(R.drawable.sample1, 3, 20210101),
@@ -29,11 +32,23 @@ class VideoList : AppCompatActivity() {
             Cctvs(R.drawable.sample2, 2, 20210805),
             Cctvs(R.drawable.sample3, 3, 202190609),
             Cctvs(R.drawable.sample1, 4, 20080612),
-            )
+            )*/
 
-        binding.items_cctv.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        /*binding.items_cctv.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         binding.items_cctv.setHasFixedSize(true)
 
-        binding.items_cctv.adapter = CctvAdapter(cctvList)
+        binding.items_cctv.adapter = CctvAdapter(cctvList)*/
+
+        binding = VideoListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //addData()
+        binding.itemsCctv.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.itemsCctv.adapter = CctvAdapter(cctvList)
+        binding.itemsCctv.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
     }
+
+    /*private fun addData() {
+        // cctv_pic, cctv_market, cctv_date 데이터를 ViewHolder에 삽입하는 코드
+
+    }*/
 }
