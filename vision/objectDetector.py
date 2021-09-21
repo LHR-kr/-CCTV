841b0b3e0abfd08579b7044da554d592ea12b888
이해랑 <reed7575@naver.com>
2021-09-22 01:34:22 +0900

    Update objectDetector.py

--- vision/objectDetector.py
+++ vision/objectDetector.py
@@ -1,15 +1,14 @@
 from darkflow.net.build import TFNet
-import json
 
 class CNN_Model:
     def __init__(self):
         #여기 나중에 수정
-        self.options = options = {"pbLoad": "../../darkflow/built_graph/my-tiny-yolo.pb", "metaLoad": "../../darkflow/built_graph/my-tiny-yolo.meta", "threshold": 0.3,
+        self.options = {"pbLoad": "../../darkflow/built_graph/my-tiny-yolo.pb", "metaLoad": "../../darkflow/built_graph/my-tiny-yolo.meta", "threshold": 0.3,
                      "labels" : "./darkflow/labels.txt"}
-        self.tfnet=TFNet(options)
+        self.tfnet=TFNet(self.options)
 
     def detect(self, image):
-        stock = {"total": 0, "Chocopie": 0, "Frenchpie": 0, "Margaret": 0, "Moncher": 0}
+        stock = {"total": 0, "chocopie": 0, "frenchpie": 0, "margaret": 0, "moncher": 0}
         result = self.tfnet.return_predict(image)
 
         person_num=0
@@ -18,13 +17,17 @@ class CNN_Model:
         for dic in result:
             name = dic.get("label")
             if name == 'Chocopie':
-                stock.update(Chocopie=(stock.get('Chocopie') + 1))
+                stock['chocopie']=stock['chocopie']+1
+
             elif name == 'Frenchpie':
-                stock.update(Frenchpie=(stock.get('Frenchpie') + 1))
+                stock['frenchpie']=stock['frenchpie']+1
+
             elif name == 'Margaret':
-                stock.update(Margaret=(stock.get('Margaret') + 1))
+                stock['margaret']=stock['margaret']+1
+
             elif name == 'Moncher':
-                stock.update(Moncher=(stock.get('Moncher') + 1))
+                stock['moncher']=stock['moncher']+1
+
             if name == "person":
                 person_num+=1
                 x=dic.get("topleft").get("x")
@@ -34,6 +37,6 @@ class CNN_Model:
                 person_bounding_boxes.append([x,y,w,h])
 
         stock.update(total=sum(stock.values()))
-        return stock, person_num, person_bounding_boxes
+        return stock, person_num, person_bounding_boxes #만약 demo 실험하면 result 적기 
 
 
